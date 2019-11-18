8213b5a723a406ebb4a16ad6b2187832e67ac223
Daniel Ochoa <ochoad24@gmail.com>
2019-11-18 11:31:01 -0600

    Creacion de modelos

---
+++ sistema/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+from django.contrib import admin
+
+
+
+class Materia(models.Model):
+    nombre = models.CharField(max_length=100)
+    def __str__(self):
+        return self.nombre
+
+class Grado(models.Model):
+    nombre = models.CharField(max_length=60)
+    seccion = models.CharField(max_length=60)
+    materias   = models.ManyToManyField(Asignacion, through='Asignacion')
+    def __str__(self):
+        return self.nombre
+
+class Asignacion (models.Model):
+    grado = models.ForeignKey(Grado, on_delete=models.CASCADE)
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
+
+class AsignacionInLine(admin.TabularInline):
+    model = Actuacion
+    extra = 1
+
+
+class MateriaAdmin(admin.ModelAdmin):
+    inlines = (AsignacionInLine)
+
+
+class GradoAdmin (admin.ModelAdmin):
+
+    inlines = (AsignacionInLine)
